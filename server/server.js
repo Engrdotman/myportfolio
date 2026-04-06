8e4949c60ce03a2d2a52ef05d4db644c369ec8be
Engrdotman <dotmanhazzan@gmail.com>
2026-04-06 08:21:07 -0700

    backend ready for deployment

--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ const cors = require("cors");
 
 const contactRoutes = require("./routes/contactRoutes");
 
+
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", contactRoutes);
 
+app.use("/api", contactRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
-});
+}); 
+
